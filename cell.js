4b7274678a3616c590ac9fa8a0a51183067c2730
GryTrean <grytrean@gmail.com>
2017-06-12 13:16:07 +0200

    Add files via upload

---
+++ cell.js
@@ -0,0 +1,90 @@
+function Cell(x, y)
+{
+	this.sides = [true, true, true, true];
+  //this.sides = [top, right, left, bottom];
+ 	this.visited = false;
+
+ 	this.fx = x;
+ 	this.fy = y;
+
+ 	this.x = x * cellSize;
+ 	this.y = y * cellSize;
+
+ 	this.cNeighbors = function()
+ 	{
+ 		var neighbors = [];
+ 		var top    = cells[index(x, y+1)];
+ 		var right  = cells[index(x+1, y)];
+ 		var left   = cells[index(x-1, y)];
+ 		var bottom = cells[index(x, y-1)];
+
+ 		if(top && !top.visited)
+ 		{
+ 			neighbors.push(top);
+ 		}
+ 		if(right && !right.visited)
+ 		{
+ 			neighbors.push(right);
+ 		}
+ 		if(left && !left.visited)
+ 		{
+ 			neighbors.push(left);
+ 		}
+ 		if(bottom && !bottom.visited)
+ 		{
+ 			neighbors.push(bottom);
+ 		}
+
+ 		if(neighbors.length > 0)
+ 		{
+ 			var r = floor(random(0, neighbors.length));
+ 			return neighbors[r];
+ 		}
+ 		else
+ 		{
+ 			return undefined;
+ 		}
+ 	}
+
+ 	this.hlight = function()
+ 	{
+ 		noStroke();
+ 		fill(255);
+ 		rect(this.x, this.y, cellSize, cellSize);
+ 		
+ 	}
+
+	this.show = function()
+	{
+		if(this.visited)
+		{
+			noStroke();
+			fill(127, 69, 125);
+			rect(this.x, this.y, cellSize, cellSize);
+		}
+		//top right left bottom
+		//top right bottom left
+		//strokeWeight(2);
+		if(this.sides[0])
+		{
+			stroke(255);
+			line(this.x, this.y, this.x+cellSize, this.y);	
+		}
+		if(this.sides[1])
+		{
+			stroke(255);
+			line(this.x+cellSize, this.y, this.x+cellSize, this.y+cellSize);
+		}
+		if(this.sides[2])
+		{
+			stroke(255);
+			line(this.x, this.y+cellSize, this.x+cellSize, this.y+cellSize);
+			
+		}
+		if(this.sides[3])
+		{
+			stroke(255);
+			line(this.x, this.y, this.x, this.y+cellSize);
+		}
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,97 @@
+var cells = [];
+var cellSize = 20;
+var cols, rows;
+var current;
+
+var stack = [];
+
+function setup()
+{
+	frameRate(5);
+	createCanvas(600, 600);
+	noStroke();
+	
+
+	cols = floor(width / cellSize);
+	rows = floor(height / cellSize);
+
+	for(var x = 0; x < rows; x++)
+	{
+		for(var y = 0; y < cols; y++)
+		{
+			
+			var cell = new Cell(x, y);
+			cells.push(cell);
+		}
+	}
+	current = cells[0];
+}
+
+function draw()
+{
+	background(55);
+	for(var i = 0; i < cells.length; i++)
+	{
+		cells[i].show();
+	}
+
+	current.visited = true;
+	current.hlight();
+	
+
+	var next = current.cNeighbors();
+	if(next)
+	{
+		next.visited = true;
+
+		stack.push(current);
+
+		
+		removeWalls(current, next);
+		
+		current = next;
+	}
+	else if(stack.length > 0)
+	{
+		current = stack.pop();
+	}
+
+
+}
+
+function index(x, y)
+{
+	if(x < 0 || y < 0 || y > cols-1 || x > rows-1)
+	{
+		return -1;
+	}
+
+	return y + x * cols;
+}
+
+function removeWalls(a, b)
+{
+	var x = a.fx - b.fx;
+	if (x === 1)
+	{
+		a.sides[3] = false;
+		b.sides[1] = false;
+	}
+	else if (x === -1)
+	{
+		a.sides[1] = false;
+		b.sides[3] = false;
+	}
+
+	var y = a.fy - b.fy;
+	if(y === 1)
+	{
+		a.sides[0] = false;
+		b.sides[2] = false;
+	}
+	else if (y === -1)
+	{
+		a.sides[2] = false;
+		b.sides[0] = false;
+	}
+}
